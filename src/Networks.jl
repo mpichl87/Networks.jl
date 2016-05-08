53ca0492fa1dd9b6cd34740d2250e2355d0379f4
Kranzi <kranzi.kranzhuber@gmail.com>
2016-05-08 20:10:54 +0200

    EDIT: Network type in all functions used

--- src/Networks.jl
+++ src/Networks.jl
@@ -43,6 +43,14 @@ end
 
 Network( sparams::Array ) = Network( sparams, [], [] )
 export Network
+const UInw = [  0.0			1.0;
+				1.0			0.0 ]
+const UImeas = [ 	√(2Z0) 	√(2Z0);
+					√2/√Z0 -√2/√Z0	]
+const UIlab = [ "U", "I" ]
+
+const UINW = Network( UInw, UImeas, UIlab )
+export UINW
 
 const UI2nw = [0.0			1.0 		0.0 		0.0;
 				1.0			0.0			0.0			0.0;
@@ -55,89 +63,8 @@ const UI2lab = [ "U", "I" ]
 const UI2NW = Network( UI2nw, UI2meas, UI2lab )
 export UI2NW
 
-
-
-# http://qucs.sourceforge.net/docs/technical/technical.pdf p9 (1.14) (1.15)
-function connect( S::Array, k::Int, T::Array, l::Int ) 
-	nS = size( S )[ 1 ]
-	nT = size( T )[ 1 ]
-	nR = nS + nT - 2
-	R = zeros( C, nR, nR )
-	Skj = S[ k, : ]
-	Tlj = T[ l, : ]
-	Sik = S[ :, k]
-	Til = T[ :, l]
- 	Skk = Skj[ k ]
-	Tll = Tlj[ l ]
-	invden = 1 / ( 1 - Skk * Tll )
-	for i in 1:k - 1
-		fak2 = Sik[ i ] * invden
-		fak1 =  Tll * fak2
-		for j in 1:k - 1
-			R[ i, 			j			] = S[ i, j ] + Skj[ j ] * fak1
-		end
-		for j in k + 1:nS
-			R[ i, 			j - 1 		] = S[ i, j ] + Skj[ j ] * fak1
-		end
-		for j in 1:l - 1
-			R[ i, 			j + nS - 1	] = Tlj[ j ] * fak2
-		end
-		for j in l + 1:nT 
-			R[ i, 			j + nS - 2 	] = Tlj[ j ] * fak2
-		end
-	end
-	for i in k + 1:nS
-		fak2 = Sik[ i ] * invden
-		fak1 =  Tll * fak2
-		for j in 1:k - 1
-			R[ i - 1, 		j 			] = S[ i, j ] + Skj[ j ] * fak1
-		end
-			for j in k + 1:nS
-			R[ i - 1,		j - 1 		] = S[ i, j ] + Skj[ j ] * fak1
-		end
-		for j in 1:l - 1
-			R[ i - 1, 		j + nS - 1 	] = Tlj[ j ] * fak2
-		end
-		for j in l + 1:nT 
-			R[ i - 1, 		j + nS - 2 	] = Tlj[ j ] * fak2
-		end
-	end
-	for i in 1:l - 1
-		fak1 = Til[ i ] * invden 
-		fak2 = Skk * fak1
-		for j in 1:k - 1
-			R[ i + nS - 1, 	j 			] = Skj[ j ]  * fak1
-		end
-		for j in k + 1:nS
-			R[ i + nS - 1, 	j - 1 		] = Skj[ j ]  * fak1
-		end
-		for j in 1:l - 1
-			R[ i + nS - 1, 	j + nS - 1	] = T[ i, j ] + Tlj[ j ] * fak2
-		end
-		for j in l + 1:nT
-			R[ i + nS - 1, 	j + nS - 2 	] = T[ i, j ] + Tlj[ j ] * fak2
-		end
-		
-	end
-	for i in l + 1:nT
-		fak1 = Til[ i ] * invden 
-		fak2 = Skk * fak1
-		for j in 1:k - 1
-			R[ i + nS - 2, 	j 			] = Skj[ j ] * fak1
-		end
-		for j in k + 1:nS
-			R[ i + nS - 2, 	j - 1 		] = Skj[ j ] * fak1
-		end
-		for j in 1:l - 1
-			R[ i + nS - 2, 	j + nS - 1	] = T[ i, j ] + Tlj[ j ] * fak2
-		end
-		for j in l + 1:nT
-			R[ i + nS - 2, 	j + nS - 2 	] = T[ i, j ] + Tlj[ j ] * fak2
-		end
-		
-	end
-	R
-end
+const TEENW = Network( Tee )
+export TEENW
 
 function connect( SN::Network, k::Int, TN::Network, l::Int ) 
 	S = SN.sparams
@@ -241,7 +168,6 @@ function connect( SN::Network, k::Int, TN::Network, l::Int )
 			measures[ i, 	j + nS - 2 ] = Tlj[ j ] * fak2
 		end
 	end
-	
 	for i in 1:nB
 		fak1 = Bil[ i ] * invden 
 		fak2 = Skk * fak1
@@ -259,74 +185,8 @@ function connect( SN::Network, k::Int, TN::Network, l::Int )
 		end
 		
 	end
-
-	measures 	= R
 	labels 		= [ S.labels; 	T.labels ]
 	Network( sparams, measures, labels )
-end
-
-
-# http://qucs.sourceforge.net/docs/technical/technical.pdf p10 (1.16)
-function connect( S::Array, k::Int, l::Int ) 
-	if k == l
-		S
-	else
-		if k > l
-			l, k = k, l
-		end
-		nS = size( S )[ 1 ]
-		nR = nS - 2
-		R = zeros( C, nR, nR )
-		Skj = S[ k, : ]
-		Sik = S[ :, k]
-	 	Slj = S[ l, : ]
-		Sil = S[ :, l]
-		Skl = Skj[ l ]
-	 	Skk = Skj[ k ]
-		Sll = Slj[ l ]
-		Slk = Slj[ k ]
-		invden = 1 / ( 1 - Skl ) * ( 1 - Slk ) - Skk * Sll
-		for i in 1:k - 1
-			fak1 = Sll * Sik[ i ] + Sil[ i ] * ( 1 - Slk )
-			fak2 = Skk * Sil[ i ] + Sik[ i ] * ( 1 - Skl )
-			for j in 1:k - 1
-				R[ i, j 	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
-			end
-			for j in k + 1:l - 1
-				R[ i, j - 1 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
-			end
-			for j in l + 1:nS
-				R[ i, j - 2 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
-			end
-		end
-		for i in k + 1:l - 1
-			fak1 = Sll * Sik[ i ] + Sil[ i ] * ( 1 - Slk )
-			fak2 = Skk * Sil[ i ] + Sik[ i ] * ( 1 - Skl )
-			for j in 1:k - 1
-				R[ i - 1, j 	] =	( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
-			end
-			for j in k + 1:l - 1
-				R[ i - 1, j - 1 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
-			end
-			for j in l + 1:nS
-				R[ i - 1, j - 2 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
-			end
-		end
-		for i in l + 1:nS
-			fak1 = Sll * Sik[ i ] + Sil[ i ] * ( 1 - Slk )
-			fak2 = Skk * Sil[ i ] + Sik[ i ] * ( 1 - Skl )
-			for j in 1:k - 1
-				R[ i - 2, j 	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
-			end
-			for j in k + 1:l - 1
-				R[ i - 2, j - 1	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
-			end
-			for j in l + 1:nS
-				R[ i - 2, j - 2	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
-			end
-		end
-		R
-	end
 end
 
 function connect( SN::Network, k::Int, l::Int ) 
@@ -339,7 +199,7 @@ function connect( SN::Network, k::Int, l::Int )
 		S = SN.sparams
 		nS = size( S )[ 1 ]
 		nR = nS - 2
-		R = zeros( C, nR, nR )
+		sparams = zeros( C, nR, nR )
 		Skj = S[ k, : ]
 		Sik = S[ :, k]
 	 	Slj = S[ l, : ]
@@ -353,45 +213,44 @@ function connect( SN::Network, k::Int, l::Int )
 			fak1 = Sll * Sik[ i ] + Sil[ i ] * ( 1 - Slk )
 			fak2 = Skk * Sil[ i ] + Sik[ i ] * ( 1 - Skl )
 			for j in 1:k - 1
-				R[ i, j 	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				sparams[ i, j 	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 			for j in k + 1:l - 1
-				R[ i, j - 1 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				sparams[ i, j - 1 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 			for j in l + 1:nS
-				R[ i, j - 2 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				sparams[ i, j - 2 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 		end
 		for i in k + 1:l - 1
 			fak1 = Sll * Sik[ i ] + Sil[ i ] * ( 1 - Slk )
 			fak2 = Skk * Sil[ i ] + Sik[ i ] * ( 1 - Skl )
 			for j in 1:k - 1
-				R[ i - 1, j 	] =	( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				sparams[ i - 1, j 	] =	( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 			for j in k + 1:l - 1
-				R[ i - 1, j - 1 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				sparams[ i - 1, j - 1 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 			for j in l + 1:nS
-				R[ i - 1, j - 2 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				sparams[ i - 1, j - 2 ] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 		end
 		for i in l + 1:nS
 			fak1 = Sll * Sik[ i ] + Sil[ i ] * ( 1 - Slk )
 			fak2 = Skk * Sil[ i ] + Sik[ i ] * ( 1 - Skl )
 			for j in 1:k - 1
-				R[ i - 2, j 	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				sparams[ i - 2, j 	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 			for j in k + 1:l - 1
-				R[ i - 2, j - 1	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				sparams[ i - 2, j - 1	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 			for j in l + 1:nS
-				R[ i - 2, j - 2	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				sparams[ i - 2, j - 2	] = ( S[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 		end
-		sparams = R
 		A = SN.measures
 		nA = size( A )[ 1 ]
-		R = zeros( C, nA, nR )
+		measures = zeros( C, nA, nR )
 		Aik = A[ :, k]
 	 	Ail = A[ :, l]
 		
@@ -399,29 +258,26 @@ function connect( SN::Network, k::Int, l::Int )
 			fak1 = Sll * Aik[ i ] + Ail[ i ] * ( 1 - Slk )
 			fak2 = Skk * Ail[ i ] + Aik[ i ] * ( 1 - Skl )
 			for j in 1:k - 1
-				R[ i, j 	] = ( A[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				measures[ i, j 	] = ( A[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 			for j in k + 1:l - 1
-				R[ i, j - 1	] = ( A[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				measures[ i, j - 1	] = ( A[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 			for j in l + 1:nS
-				R[ i, j - 2	] = ( A[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
+				measures[ i, j - 2	] = ( A[ i, j ] + Skj[ j ] * fak1 +	Slj[ j ] * fak2 ) * invden
 			end
 		end
-		
-		measures 	= R
 		labels 		= SN.labels
 		Network( sparams, measures, labels )
 	end
 end
-
 export connect
 
-instr_shunt( sh ) 	= connect( sh, 1, UI, 2 )
-instr_through( th ) = connect( connect( th, 1, UI2, 2 ), 1, 3 )
+instr_shunt( sh ) 	= connect( sh, 1, UINW, 2 )
+instr_through( th ) = connect( connect( th, 1, UI2NW, 2 ), 1, 3 )
 export instr_shunt, instr_through
 
-parallel( sh1, sh2 ) = connect( connect( Tee, 1, sh1, 1 ), 1, sh2, 1 )
+parallel( sh1, sh2 ) = connect( connect( TEENW, 1, sh1, 1 ), 1, sh2, 1 )
 serial( th, sh )   	 = connect( th, 2, sh, 1 )
 export parallel, serial
 
