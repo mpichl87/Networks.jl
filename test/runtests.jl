7788b62a7f3b2105f8ac8338e7adfddc71b288bc
Michael Pichler <michael.pichler@meduniwien.ac.at>
2016-05-17 14:31:29 +0200

    TEST: error in unittests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,9 @@ using Networks
 @test shunt( Z0 ) 	== Network( reshape( [ 0.0 ], 1, 1 ) )
 @test shunt( 0.0 )  == Network( reshape( [ -1.0 ], 1, 1 ) )
 
-@test through( 0 ) 	== Network( [ 	0 1;
-									1 0 ] )
+@test through( 0 ) 	== 1;
+# Network( [ 	0 1;
+# 									1 0 ] )
 
 @test Networks.connect( through( 0.0 ), 2, shunt( Z0 ), 1 ) == Network( shunt( Z0 ) )
 
