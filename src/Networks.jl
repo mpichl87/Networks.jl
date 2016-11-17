b6ff3a05a0be29a79126092cffcc8056ab7ef20a
Michael Pichler <michael.pichler@meduniwien.ac.at>
2016-11-17 17:37:34 +0100

    got rid of warnings

--- src/Networks.jl
+++ src/Networks.jl
@@ -1,5 +1,5 @@
 __precompile__()
-module Networks 
+module Networks
 importall Base
 
 const Z0 = 50.0
@@ -8,8 +8,8 @@ export Z0
 type Network{ T <: Number }
 	sparams::Array{ T, 2 }
 	measures::Array{ T, 2 }
-	labels::Array{ ASCIIString, 1 }
-	function Network{ T }( sparams::Array{ T, 2 }, measures::Array{ T, 2 }, labels::Array{ ASCIIString, 1 } )
+	labels::Array{ String, 1 }
+	function Network{ T }( sparams::Array{ T, 2 }, measures::Array{ T, 2 }, labels::Array{ String, 1 } )
 		srows = size( sparams, 1 )
 		scols = srows == 0 ? 0 : size( sparams, 2 )
 		mrows = size( measures, 1 )
@@ -21,26 +21,26 @@ type Network{ T <: Number }
 		new( sparams, measures, labels )
 	end
 end
-function Network{ T }( sparams::Array{ T, 2 }, measures::Array{ T, 2 }, labels::Array{ ASCIIString, 1 } ) 
+function Network{ T }( sparams::Array{ T, 2 }, measures::Array{ T, 2 }, labels::Array{ String, 1 } )
 	Network{ T }( sparams, measures, labels )
 end
-function Network{ S, T }( sparams::Array{ S, 2 }, measures::Array{ T, 2 }, labels::Array{ ASCIIString, 1 } )
+function Network{ S, T }( sparams::Array{ S, 2 }, measures::Array{ T, 2 }, labels::Array{ String, 1 } )
 	sparams, measures = promote( sparams, measures )
 	Network{ typeof( sparams ) }( sparams, measures, labels )
 end
 function Network{ T }( sparams::Array{ T, 2 } )
-	Network{ T }( sparams, Array{ T, 2 }(), ASCIIString[] ) 
+	Network{ T }( sparams, Array{ T, 2 }(), String[] )
 end
 export Network
 
 function ==( nw1::Network, nw2::Network )
 	ok = true
-	ok &= nw1.sparams == nw2.sparams 
+	ok &= nw1.sparams == nw2.sparams
 	ok &= ( length( nw1.measures ) == 0 && length( nw2.measures ) == 0 ) || nw1.measures == nw2.measures
 	ok &= nw1.labels == nw2.labels
 end
 
-function measure( S::Network, label::ASCIIString )
+function measure( S::Network, label::String )
 	for ( i, l ) in enumerate( S.labels )
 		if l == label
 			return S.measures[ i, : ]
@@ -49,19 +49,19 @@ function measure( S::Network, label::ASCIIString )
 end
 export measure
 
-voltage( S::Network, component::ASCIIString ) = measure( S, "$( component )-U")
+voltage( S::Network, component::String ) = measure( S, "$( component )-U")
 voltage( S::Network ) = component -> voltage( S, component )
 export  voltage
 
-current( S::Network, component::ASCIIString ) = measure( S, "$( component )-I")
+current( S::Network, component::String ) = measure( S, "$( component )-I")
 current( S::Network ) = component -> current( S, component )
 export current
 
-power( S::Network, component::ASCIIString ) = voltage( S, component ) * conj( current( S, component ) ) / 2
+power( S::Network, component::String ) = voltage( S, component ) * conj( current( S, component ) ) / 2
 power( S::Network ) = component -> power( S, component )
 export power
 
-function UI( component::ASCIIString = "" )
+function UI( component::String = "" )
 	UIsp = [  	0.0	1.0;
 				1.0	0.0 ]
 	UImeas = [ 	√(2Z0) 	√(2Z0);
@@ -71,18 +71,18 @@ function UI( component::ASCIIString = "" )
 end
 export UI
 
-function UI2( component::ASCIIString = "" )
+function UI2( component::String = "" )
 	UI2sp = [ 	0.0			1.0 		0.0 		0.0;
 				1.0			0.0			0.0			0.0;
 				0.0			0.0			0.0			1.0;
 				0.0			0.0			1.0			0.0 ]
-	UI2meas = [ 	√(2Z0) 		√(2Z0)		-√(2Z0) 	-√(2Z0);  
+	UI2meas = [ 	√(2Z0) 		√(2Z0)		-√(2Z0) 	-√(2Z0);
 					1/√(2Z0)	-1/√(2Z0)	1/√(2Z0)	-1/√(2Z0)	]
 	UI2lab = [ "$( component )-U", "$( component )-I" ]
 	Network( UI2sp, UI2meas, UI2lab )
 end
 export UI2
- 
+
 const TEEsp = [	-1.0/3  2.0/3  2.0/3;
 				 2.0/3 -1.0/3  2.0/3;
 				 2.0/3  2.0/3 -1.0/3 ]
@@ -97,7 +97,7 @@ through( z, z0 ) = Network( 1 / ( z + 2z0 ) * [ z 2z0; 2z0 z ] )
 through( z ) = through( z, Z0 )
 export through
 
-function connect{ S, T }( SN::Network{ S }, k::Int, TN::Network{ T }, l::Int ) 
+function connect{ S, T }( SN::Network{ S }, k::Int, TN::Network{ T }, l::Int )
 	S, T = promote( SN.sparams, TN.sparams )
 	nS = size( S )[ 1 ]
 	nT = size( T )[ 1 ]
@@ -122,7 +122,7 @@ function connect{ S, T }( SN::Network{ S }, k::Int, TN::Network{ T }, l::Int )
 		for j in 1:l - 1
 			sparams[ i, 			j + nS - 1	] = Tlj[ j ] * fak2
 		end
-		for j in l + 1:nT 
+		for j in l + 1:nT
 			sparams[ i, 			j + nS - 2 	] = Tlj[ j ] * fak2
 		end
 	end
@@ -138,12 +138,12 @@ function connect{ S, T }( SN::Network{ S }, k::Int, TN::Network{ T }, l::Int )
 		for j in 1:l - 1
 			sparams[ i - 1, 		j + nS - 1 	] = Tlj[ j ] * fak2
 		end
-		for j in l + 1:nT 
+		for j in l + 1:nT
 			sparams[ i - 1, 		j + nS - 2 	] = Tlj[ j ] * fak2
 		end
 	end
 	for i in 1:l - 1
-		fak1 = Til[ i ] * invden 
+		fak1 = Til[ i ] * invden
 		fak2 = Skk * fak1
 		for j in 1:k - 1
 			sparams[ i + nS - 1, 	j 			] = Skj[ j ]  * fak1
@@ -159,7 +159,7 @@ function connect{ S, T }( SN::Network{ S }, k::Int, TN::Network{ T }, l::Int )
 		end
 	end
 	for i in l + 1:nT
-		fak1 = Til[ i ] * invden 
+		fak1 = Til[ i ] * invden
 		fak2 = Skk * fak1
 		for j in 1:k - 1
 			sparams[ i + nS - 2, 	j 			] = Skj[ j ] * fak1
@@ -193,7 +193,7 @@ function connect{ S, T }( SN::Network{ S }, k::Int, TN::Network{ T }, l::Int )
 			for j in 1:l - 1
 				measures[ i, 	j + nS - 1 ] = Tlj[ j ] * fak2
 			end
-			for j in l + 1:nT 
+			for j in l + 1:nT
 				measures[ i, 	j + nS - 2 ] = Tlj[ j ] * fak2
 			end
 		end
@@ -201,7 +201,7 @@ function connect{ S, T }( SN::Network{ S }, k::Int, TN::Network{ T }, l::Int )
 	if nB > 0
 		Bil = B[ :, l]
 		for i in 1:nB
-			fak1 = Bil[ i ] * invden 
+			fak1 = Bil[ i ] * invden
 			fak2 = Skk * fak1
 			for j in 1:k - 1
 				measures[ i + nA, j 			] = Skj[ j ] * fak1
@@ -215,14 +215,14 @@ function connect{ S, T }( SN::Network{ S }, k::Int, TN::Network{ T }, l::Int )
 			for j in l + 1:nT
 				measures[ i + nA, j + nS - 2 	] = B[ i, j ] + Tlj[ j ] * fak2
 			end
-			
+
 		end
 	end
 	labels 		= [ SN.labels; 	TN.labels ]
 	Network( sparams, measures, labels )
 end
 
-function connect( SN::Network, k::Int, l::Int ) 
+function connect( SN::Network, k::Int, l::Int )
 	if k == l
 		SN
 	else
@@ -286,7 +286,7 @@ function connect( SN::Network, k::Int, l::Int )
 		measures = zeros( eltype( A ), nA, nR )
 		Aik = A[ :, k]
 	 	Ail = A[ :, l]
-		
+
 		for i in 1:nA
 			fak1 = Sll * Aik[ i ] + Ail[ i ] * ( 1 - Slk )
 			fak2 = Skk * Ail[ i ] + Aik[ i ] * ( 1 - Skl )
@@ -306,13 +306,13 @@ function connect( SN::Network, k::Int, l::Int )
 end
 export connect
 
-function labels!( nw::Network, prefix::ASCIIString )
+function labels!( nw::Network, prefix::String )
 	labels = map( l -> "$( prefix )$( l )", nw.labels )
 	Network( nw.sparams, nw.measures, labels )
 end
 
-instr_shunt(   sh::Network, component::ASCIIString = "" ) = connect( sh, 1, UI( component ), 2 )
-instr_through( th::Network, component::ASCIIString = "" ) = connect( connect( th, 1, UI2( component ), 2 ), 1, 3 )
+instr_shunt(   sh::Network, component::String = "" ) = connect( sh, 1, UI( component ), 2 )
+instr_through( th::Network, component::String = "" ) = connect( connect( th, 1, UI2( component ), 2 ), 1, 3 )
 export instr_shunt, instr_through
 
 parallel( sh1::Network, sh2::Network ) = connect( connect( Tee, 1, sh1, 1 ), 1, sh2, 1 )
@@ -345,11 +345,11 @@ y_c( c, ω ) = ω * c * 1im
 z_c( c, ω ) = z( y_c( c, ω ) )
 export y_c, z_c
 
-rcl( r, c, l, ω ) = 
-	ser( 
+rcl( r, c, l, ω ) =
+	ser(
 		par( r, z_c( c, ω ) ),
 		z_l( l, ω )
-	)  
+	)
 
 rcl( r, c, l ) = ω -> rcl( r, c, l, ω )
 export rcl
@@ -359,9 +359,9 @@ cap( c, r, ω ) = ser( r, z_c( c, ω ) )
 ind( l, r, ω ) = ser( r, z_l( l, ω ) )
 export cap, ind
 
-function pin( Rsf, Ct ) 
+function pin( Rsf, Ct )
 	return function ( state, ω )
-		state ? Rsf : z_c( Ct, ω )	
+		state ? Rsf : z_c( Ct, ω )
 	end
 end
 export pin
