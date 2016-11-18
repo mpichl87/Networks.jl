5884f68af6d6051f6e5a08b9cb17ac67bb8547b8
Michael Pichler <michael.pichler@meduniwien.ac.at>
2016-11-18 13:18:04 +0100

    NEW: doc

--- src/Networks.jl
+++ src/Networks.jl
@@ -3,7 +3,7 @@ module Networks
 importall Base
 
 const Z0 = 50.0
-export Z0 
+export Z0
 
 type Network{ NumType <: Number }
 	sparams::Array{ NumType, 2 }
@@ -33,6 +33,11 @@ function Network{ NumType }( sparams::Array{ NumType, 2 } )
 end
 export Network
 
+"""
+		==( nw1, nw2 )
+
+Test equality of two networks.
+"""
 function ==( nw1::Network, nw2::Network )
 	ok = true
 	ok &= nw1.sparams == nw2.sparams
@@ -40,6 +45,11 @@ function ==( nw1::Network, nw2::Network )
 	ok &= nw1.labels == nw2.labels
 end
 
+"""
+		measure( S, label )
+
+Extracts labeled measurement from network S.
+"""
 function measure( S::Network, label::String )
 	for ( i, l ) in enumerate( S.labels )
 		if l == label
@@ -49,18 +59,38 @@ function measure( S::Network, label::String )
 end
 export measure
 
+"""
+		voltage( S, component )
+
+Extracts  voltage measurement labeled with component from network S.
+"""
 voltage( S::Network, component::String ) = measure( S, "$( component )-U")
 voltage( S::Network ) = component -> voltage( S, component )
 export  voltage
 
+"""
+		current( S, component )
+
+Extracts  current measurement labeled with component from network S.
+"""
 current( S::Network, component::String ) = measure( S, "$( component )-I")
 current( S::Network ) = component -> current( S, component )
 export current
 
+"""
+		power( S, component )
+
+Computes  power measurement from voltage and current labeled with component from network S.
+"""
 power( S::Network, component::String ) = voltage( S, component ) * conj( current( S, component ) ) / 2
 power( S::Network ) = component -> power( S, component )
 export power
 
+"""
+		UI( component )
+
+Generates voltage and current measurement ports labeled with component for one port.
+"""
 function UI( component::String = "" )
 	UIsp = [  	0.0	1.0;
 				1.0	0.0 ]
@@ -71,6 +101,11 @@ function UI( component::String = "" )
 end
 export UI
 
+"""
+		UI2( component )
+
+Generates voltage and current measurement ports labeled with component for two ports.
+"""
 function UI2( component::String = "" )
 	UI2sp = [ 	0.0			1.0 		0.0 		0.0;
 				1.0			0.0			0.0			0.0;
@@ -82,21 +117,48 @@ function UI2( component::String = "" )
 	Network( UI2sp, UI2meas, UI2lab )
 end
 export UI2
+"""
+		TEEsp
 
+S-parameters for 3-port tee.
+"""
 const TEEsp = [	-1.0/3  2.0/3  2.0/3;
-				 2.0/3 -1.0/3  2.0/3;
-				 2.0/3  2.0/3 -1.0/3 ]
+								 2.0/3 -1.0/3  2.0/3;
+				 				 2.0/3  2.0/3 -1.0/3 ]
+
+"""
+		TEEsp
+
+3-port tee network.
+"""
 const Tee = Network( TEEsp )
 export Tee
 
+"""
+		shunt( z[, z0] )
+
+Generates 1-port shunt network of a shunted impedanze z.
+z0 is the system impedance with a default value of 50 Ohms.
+"""
 shunt( z, z0 ) = Network( reshape( [ ( z - z0 ) / ( z + z0 ) ], 1, 1 ) )
 shunt( z ) = shunt( z, Z0 )
 export shunt
 
+"""
+		through( z[, z0] )
+
+Generates 2-port through network from an impedanze z.
+z0 is the system impedance with a default value of 50 Ohms.
+"""
 through( z, z0 ) = Network( 1 / ( z + 2z0 ) * [ z 2z0; 2z0 z ] )
 through( z ) = through( z, Z0 )
 export through
 
+"""
+		connect( N1, k, N2, l )
+
+Connects port k of network N1 with port l of Network N2 and returns the resulting network.
+"""
 function connect{ NumType1, NumType2 }( N1::Network{ NumType1 }, k::Int, N2::Network{ NumType2 }, l::Int )
 	S, T = promote( N1.sparams, N2.sparams )
 	nS = size( S )[ 1 ]
@@ -222,6 +284,10 @@ function connect{ NumType1, NumType2 }( N1::Network{ NumType1 }, k::Int, N2::Net
 	Network( sparams, measures, labels )
 end
 
+"""n		connect( SN, k, l )
+
+Connects port k of network SN with port l of the same network and returns the resulting network.
+"""
 function connect( SN::Network, k::Int, l::Int )
 	if k == l
 		SN
@@ -306,42 +372,136 @@ function connect( SN::Network, k::Int, l::Int )
 end
 export connect
 
-function labels!( nw::Network, prefix::String )
+"""
+		prefix_labels( nw, prefix )
+
+Prefixes all labels of a network with a common prefix.
+"""
+function prefix_labels!( nw::Network, prefix::String )
 	labels = map( l -> "$( prefix )$( l )", nw.labels )
 	Network( nw.sparams, nw.measures, labels )
 end
 
+"""
+		instr_shunt( sh, component )
+
+Instrumentates a shunt 1-port network. Adds voltage and current measurement ports labeled with component.
+"""
 instr_shunt(   sh::Network, component::String = "" ) = connect( sh, 1, UI( component ), 2 )
+
+"""
+		instr_through( sh, component )
+
+Instrumentates a through 2-port network. Adds voltage and current measurement ports labeled with component.
+"""
 instr_through( th::Network, component::String = "" ) = connect( connect( th, 1, UI2( component ), 2 ), 1, 3 )
 export instr_shunt, instr_through
 
+"""
+		parallel( sh1, sh2 )
+
+Connects two shunt networks in parallel.
+"""
 parallel( sh1::Network, sh2::Network ) = connect( connect( Tee, 1, sh1, 1 ), 1, sh2, 1 )
+
+"""
+		serial( th, sh )
+
+Connects a through network in series to a shunt network.
+"""
 serial(    th::Network,  sh::Network ) = connect( th, 2, sh, 1 )
 export parallel, serial
 
+"""
+		y( z )
+
+Calculates the admitance from an impedance.
+"""
 y( z ) = 1 / z
+
+"""
+		z( y )
+
+Calculates the impedance from an admitance.
+"""
 z( y ) = 1 / y
 export y, z
 
+"""
+		ser_z( z1, z2 )
+
+Calculates the impedance of a serial connection of two impedances.
+"""
 ser_z( z1, z2 ) = z1 + z2
+
+"""
+		par_y( y1, y2 )
+
+Calculates the admitance of a parallel connection of two admitances.
+"""
 par_y( y1, y2 ) = y1 + y2
 ser = ser_z
 export ser_z, par_y, ser
 
+"""
+		par_z( z1, z2 )
+
+Calculates the impedance of a parallel connection of two impedances.
+"""
 par_z( z1, z2 ) = z( par_y( y( z1 ), y( z2 ) ) )
+
+"""
+		ser_y( y1, y2 )
+
+Calculates the admitance of a serial connection of two admitances.
+"""
 ser_y( y1, y2 ) = y( ser_z( z( y1 ), z( y2 ) ) )
 par = par_z
 export par_z, ser_y, par
 
+"""
+		ω( f )
+
+Calculates the angular frequency from frequency.
+"""
 ω( f ) = 2π * f
+
+"""
+		f( ω )
+
+Calculates the frequency from angular frequency.
+"""
 f( ω ) = ω / 2π
 export ω, f
 
+"""
+		z_l( l, ω )
+
+Calculates the impedance of an ideal inductor with inductance l at the  angular frequency ω.
+"""
 z_l( l, ω ) = ω * l * 1im
+
+
+"""
+		y_l( l, ω )
+
+Calculates the admitance of an ideal inductor with inductance l at the  angular frequency ω.
+"""
 y_l( l, ω ) = y( z_l( l, ω ) )
 export z_l, y_l
 
+"""
+		y_c( c, ω )
+
+Calculates the admitance of an ideal capacitor with capacitance c at the  angular frequency ω.
+"""
 y_c( c, ω ) = ω * c * 1im
+
+"""
+		z_c( c, ω )
+
+Calculates the impedance of an ideal capacitor with capacitance c at the  angular frequency ω.
+"""
 z_c( c, ω ) = z( y_c( c, ω ) )
 export y_c, z_c
 
